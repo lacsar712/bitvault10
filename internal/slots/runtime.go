3692bf3cd542c4c49e27e9967648d894c4c57b7a
lacsar712 <lacsar712@users.noreply.github.com>
2026-08-19 03:35:47 +0800

    test model fix bug175: bitvault10

--- internal/slots/runtime.go
+++ internal/slots/runtime.go
@@ -127,6 +127,8 @@ func (img *Image) SeriesName() string {
 
 func ParseManifest(b []byte) (map[string]int, error) {
 	var m map[string]int
-	_ = json.Unmarshal(b, &m)
+	if err := json.Unmarshal(b, &m); err != nil {
+		return nil, err
+	}
 	return m, nil
 }
